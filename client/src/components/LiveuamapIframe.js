0c18142e6ff5e0902af38f6531fcc2f9e29907b7
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-10-13 20:22:30 +0100

    Add the Frame

---
+++ client/src/components/LiveuamapIframe.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LiveuamapIframe = () => {
+  return (
+    <iframe
+      style={{ width: "1200px", height: "500px", border: "0px" }}
+      src="https://liveuamap.com"
+      title="Liveuamap"
+    ></iframe>
+  );
+};
+
+export default LiveuamapIframe;
