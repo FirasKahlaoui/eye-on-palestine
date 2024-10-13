397ff476b9bb71a2141129976a9941edf1147890
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-10-13 01:29:10 +0100

    Try usertime line

--- server/routes/twitterRoutes.js
+++ server/routes/twitterRoutes.js
@@ -1,25 +1,28 @@
-const express = require('express');
-const router = express.Router();
-const Twitter = require('twitter');
+const express = require("express");
+const Twitter = require("twitter");
 
-// Initialize Twitter client
+const router = express.Router();
+
 const client = new Twitter({
   consumer_key: process.env.TWITTER_API_KEY,
   consumer_secret: process.env.TWITTER_API_SECRET_KEY,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
-// Define a route to fetch tweets
-router.get('/tweets', (req, res) => {
-  client.get('statuses/user_timeline', { screen_name: 'nodejs', count: 10 }, function(error, tweets, response) {
-    if (error) {
-      console.error('Error fetching tweets:', error);
-      res.status(500).send('Error fetching tweets');
-    } else {
-      res.json(tweets);
-    }
-  });
+// Fetch tweets from a specific user
+router.get("/user/:username", async (req, res) => {
+  const { username } = req.params;
+  try {
+    const tweets = await client.get("statuses/user_timeline", {
+      screen_name: username,
+      count: 5,
+    });
+    res.status(200).json(tweets);
+  } catch (error) {
+    console.error("Error fetching tweets:", error);
+    res.status(500).send("Error fetching tweets: " + JSON.stringify(error));
+  }
 });
 
 module.exports = router;
