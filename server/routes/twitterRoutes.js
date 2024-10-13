bdf1c8e0cdf2ce0cdcdf13ef0592e214637954ae
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-10-13 01:35:55 +0100

    Connect to the endpoint

--- server/routes/twitterRoutes.js
+++ server/routes/twitterRoutes.js
@@ -1,21 +1,18 @@
 const express = require("express");
-const Twitter = require("twitter");
+const Twitter = require("twitter-v2");
 
 const router = express.Router();
 
 const client = new Twitter({
-  consumer_key: process.env.TWITTER_API_KEY,
-  consumer_secret: process.env.TWITTER_API_SECRET_KEY,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  bearer_token: process.env.TWITTER_BEARER_TOKEN,
 });
 
 // Fetch user details by username
 router.get("/user/:username", async (req, res) => {
   const { username } = req.params;
   try {
-    const user = await client.get("users/by/username/" + username);
-    res.status(200).json(user);
+    const { data } = await client.get(`users/by/username/${username}`);
+    res.status(200).json(data);
   } catch (error) {
     console.error("Error fetching user:", error);
     res.status(500).send("Error fetching user: " + JSON.stringify(error));
